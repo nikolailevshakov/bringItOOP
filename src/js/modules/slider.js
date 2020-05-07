44dea877eecd46b13e1fabd242c08f9dd73ab235
Levshakov Nikolai <levshakov_nikk@bk.ru>
2020-05-07 13:36:49 +0300

    add popup window

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -13,6 +13,22 @@ export default class Slider {
         if (n<1) {
             this.slideIndex = this.slides.length;
         }
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+
+            }
+        } catch (e) {}
+
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
@@ -24,6 +40,11 @@ export default class Slider {
     }
 
     render() {
+// пытается выполнится блок в трай, если не получается выполняется кэтч и код не ломается
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch(e) {}
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
@@ -38,4 +59,5 @@ export default class Slider {
 
         this.showSlides(this.slideIndex);
     }
+
 }
